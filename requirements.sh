508d233d3ddcc972708ae5397ba55c13f5be4d0e
Michael Chow <chowmichedu@gmail.com>
2018-04-20 16:47:32 -0400

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -6,8 +6,11 @@ set -e
 apt-get update
 apt-get install -y libcurl4-openssl-dev libxml2-dev
 
+
 mkdir -p /home/repl/datasets
-mv kaggle-survey-2017.zip /home/repl/datasets
 cd /home/repl/datasets
+curl http://s3.amazonaws.com/assets.datacamp.com/production/repositories/2803/datasets/30e11661e2452c14f473ecf84fcf03a8eaa77006/kaggle-survey-2017.zip \
+       -O
+
 unzip kaggle-survey-2017.zip
 chmod a+rw ./*
